22fc1200ea0f26b4abdcbba75607969876842e21
mahabubrabby <21201093@uap-bd.edu>
2026-07-14 22:51:20 +0600

    fix: resolve preloader path case in CategoryPage and remaining pages

--- client/super-admin/src/page/CategoryPage.jsx
+++ client/super-admin/src/page/CategoryPage.jsx
@@ -1,5 +1,5 @@
 import MasterLayout from "../layout/Masterlayout"
-import Preloader from "../helper/Preloader";
+import Preloader from "../helper/preloader";
 import Category from "../components/Category"
 
 const CategoryPage = () => {
